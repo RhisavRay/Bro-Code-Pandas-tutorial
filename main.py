37b52bb420dc6ef9526711723d9fa05609ddc1a0
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-10 22:45:43 +0530

    Basic Series theory

--- main.py
+++ main.py
@@ -11,4 +11,27 @@ Series: A pandas 1-dimensional labeled array that can store any data type.
 data = [100, 101, 102, 103]
 series_A = pandas.Series(data)
 print(series_A)
+"""
+In the above lines of code, a list of integers is converted to a Series using pandas.
+Upon printing the series, we see the following result:
 
+0    100
+1    101
+2    102
+3    103
+dtype: int64
+
+The first column is for index with a label(remember. Will come handy later), and the next column is where our data got stored.
+After the data gets printed, the meta-data of the series is also printed below. This changes based on the data type being stored in the
+series.
+For example, for all integer values, it shows
+dtype: int64
+For any float value in the mix, it shows
+dtype: float64
+For any character value in the mix, it shows
+dtype: object
+For all boolean values, it shows
+dtype: bool
+
+NOTE: Series() is not a method. It is a constructor
+"""
