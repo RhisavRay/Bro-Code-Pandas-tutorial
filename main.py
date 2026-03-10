9d5756f4a00de82b7dc1e5c157130b652c45e15e
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-10 22:08:39 +0530

    Started with Series

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas
+
+# print(pandas.__version__)
+# The above statement can be used to check the version of pandas installed in our system\
+
+# Series: A pandas 1-dimensional labeled array that can store any data type.
+#         You can think of it as a single column in a spreadsheet
+
