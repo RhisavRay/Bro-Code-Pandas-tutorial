7ce539ab6f7801996b3b414b81f7ec163bcedd97
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-25 15:56:57 +0530

    Added a new row

--- dataframe.py
+++ dataframe.py
@@ -36,3 +36,20 @@ Take it this way. Consider that dataframe_B is like a dictionary, just like data
 key-value pair in a dictionary? Similarly we do here. It would give the same result as adding the key-value pair of "Job" and the list of
 jobs to data_A itself using a similar syntax.
 """
+
+# Add a new row
+# new_rows = pandas.DataFrame([{"Name": "Sandy", "Age": 35, "Job": "Scientist"},
+#                             {"Name": "Crabs", "Age": 54, "Job": "Manager"}],
+#                            index=["Employee 5", "Employee 6"])
+
+# new_rows = pandas.DataFrame([{"Name": ["Sandy", "Crabs"], "Age": [35, 54], "Job": ["Scientist", "Manager"]}], index=["Employee 5", "Employee 6"])
+
+new_rows = {
+    "Name": ["Sandy", "Crabs"],
+    "Age": [35, 54],
+    "Job": ["Scientist", "Manager"]
+}
+new_rows_df = pandas.DataFrame(new_rows, index=["Employee 5", "Employee 6"])
+
+dataframe_B = pandas.concat([dataframe_B, new_rows_df])
+print(dataframe_B)
