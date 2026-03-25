5f8338db866fc51678c373f5c8e59c6ec1093be1
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-25 15:55:37 +0530

    Created a new data frame and did some actions

--- dataframe.py
+++ dataframe.py
@@ -4,3 +4,17 @@ import pandas
 Dataframe - It is a tabular data structure with rows and columns (2 dimensional).
             Similar to an Excel spreadsheet.
 """
+
+data_A = {
+    "Name": ["Rhisav", "Rohan", "Raima", "Rohit"],
+    "Age": [24, 23, 22, 23]
+}
+
+dataframe_A = pandas.DataFrame(data_A)
+print(dataframe_A)
+
+dataframe_B = pandas.DataFrame(data_A, index=["Employee 1", "Employee 2", "Employee 3", "Employee 4"])
+print(dataframe_B)
+
+print(dataframe_B.loc["Employee 1"])
+print(dataframe_B.iloc[2])
