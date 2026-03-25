41e0f16fbdcc145a785ba9d240f76360a71feb40
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-25 16:18:57 +0530

    Explanation added

--- importing.py
+++ importing.py
@@ -7,3 +7,12 @@ df_csv = pd.read_csv("data.csv")
 df_json = pd.read_json("data.json")
 
 print(df_json)
+
+"""
+There is nothing much in terms of importing in pandas.
+
+Just one thing. If the dataset has several rows, only first 5 ad last 5 are shown. Also if too many columns, then only first 2 and last 2
+are shown. The actual size gets mentioned below.
+
+If you do want to see the full expanded dataset, just use ".to_string()", like shown in the commented out print statement
+"""
