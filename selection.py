407334dab804a7f65a4594881a400bfda480cfb1
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-03-27 19:16:04 +0530

    Code for selection by column

--- selection.py
+++ selection.py
@@ -5,3 +5,14 @@ Selection in pandas is nothing but choosing what data to show in our output stat
 show.
 """
 
+df_1 = pd.read_csv("data.csv")
+
+# Selection by column
+print(df_1["model"])
+
+print(df_1[["make", "model", "engine_cc"]])
+
+print(df_1.iloc[:, 3:5])
+
+print(df_1.loc[:, "make":"engine_cc"])
+
