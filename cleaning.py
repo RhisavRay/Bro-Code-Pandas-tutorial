5472f16cb4cd22f198e8ee8493fe89d68806292b
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2026-04-01 18:24:22 +0530

    Data cleaning is done

--- cleaning.py
+++ cleaning.py
@@ -1,3 +1,39 @@
 import pandas as pd
 
 df = pd.read_csv('data.csv')
+
+"""
+Data cleaning is the process of fixing/removing incomplete, incorrect or irrelevant data
+"""
+
+# Drop irrelevant column(s)
+# df = df.drop(columns=['make', 'model'])
+# print(df)
+
+
+# Handle missing data
+# df = df.dropna(subset=['make'])
+# df = df.fillna({'make': 'No maker'})
+
+
+# Fix inconsistent values
+# df['type'] = df['type'].replace({'Naked': 'N',
+#                                  'Sport': 'S',
+#                                  'Touring': 'T',
+#                                  'Adventure': 'A',
+#                                  'Cruiser': 'C',
+#                                  'Dirt': 'D'})
+
+
+# Standardize text
+# df['make'] = df['make'].str.lower()
+
+
+# Fix data types
+# df['engine_cc'] = df['engine_cc'].astype(bool)
+
+
+# Remove duplicate values
+# df = df.drop_duplicates()
+
+print(df.to_string(index=False))
